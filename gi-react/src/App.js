b5f61f54041e679686eab7928d76ef52bbff3b4d
vbenavi0 <victorvill.0988@gmail.com>
2024-01-06 14:31:31 -0500

    completed very easy challenge

---
+++ gi-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>My name is Victor, I am 19 years old, and I am from El Salvador</p>
+    </div>
+  );
+}
+
+export default App;
